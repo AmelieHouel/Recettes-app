c11f6baddd5dca26722d20a1a2b15e2949616292
AmelieHouel <amelie.houel@hetic.net>
2020-05-26 10:16:56 +0200

    firs commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+// CSS
+import "./App.css";
+
+import Header from "./components/Header";
+import Admin from "./components/Admin";
+import recettes from './recettes';
+
+class App extends Component {
+  state = {
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
+  };
+
+  chargerExemple = () => this.setState({ recettes })
+
+  render() {
+    return (
+      <div className="box">
+        <Header pseudo={this.state.pseudo} />
+        <div className="cards">
+          <div className="card">
+            <h2>Une Carte</h2>
+          </div>
+        </div>
+        <Admin
+          chargerExemple={this.chargerExemple}></Admin>
+      </div>
+    );
+  }
+}
+
+export default App;
