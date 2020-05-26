83ccec79d5e808662c24c3187ea1dc1da76d21c0
AmelieHouel <amelie.houel@hetic.net>
2020-05-26 11:33:42 +0200

    add card component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from "react";
+
 // CSS
 import "./App.css";
 
+//components
 import Header from "./components/Header";
 import Admin from "./components/Admin";
-import recettes from './recettes';
+import Card from "./components/Card";
+import recettes from "./recettes";
 
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
-    recettes: {}
+    recettes: {},
   };
 
-  chargerExemple = () => this.setState({ recettes })
+  chargerExemple = () => this.setState({ recettes });
 
   render() {
+    const cards = Object.keys(this.state.recettes).map((key) => (
+      <Card key={key} details={this.state.recettes[key]} />
+    ));
+    console.log(cards);
     return (
       <div className="box">
         <Header pseudo={this.state.pseudo} />
-        <div className="cards">
-          <div className="card">
-            <h2>Une Carte</h2>
-          </div>
-        </div>
-        <Admin
-          chargerExemple={this.chargerExemple}></Admin>
+        <div className="cards">{cards}</div>
+        <Admin chargerExemple={this.chargerExemple}></Admin>
       </div>
     );
   }
