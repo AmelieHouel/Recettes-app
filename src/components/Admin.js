25a50a932d821880c723dd4f88eaa432517ad4c7
AmelieHouel <amelie.houel@hetic.net>
2020-05-28 00:00:19 +0200

    keep connexion between sessions

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -13,6 +13,14 @@ class Admin extends Component {
     chef: null
   }
 
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.handleAuth({ user })
+      }
+    })
+  }
+
   handleAuth = async authData => {
     const box = await base.fetch(this.props.pseudo, { contex: this })
 
@@ -36,8 +44,16 @@ class Admin extends Component {
     .then(this.handleAuth)
   }
 
+  logout = async () => {
+    console.log("déconnexion")
+    await firebase.auth().signOut()
+    this.setState({ uid: null })
+  }
+
   render() {
     const { recettes, ajouterRecette, majRecette, chargerExemple, supprimerRecette } = this.props;
+
+    const logout= <button onClick={this.logout}>Déconnexion</button>
 
     //Si l'utilisateur n'est pas connecté
     if (!this.state.uid) {
@@ -48,6 +64,7 @@ class Admin extends Component {
       return (
         <div>
           <p>Tu n'es pas le chef de cette boite !</p>
+          {logout}
         </div>
       )
     }
@@ -66,6 +83,7 @@ class Admin extends Component {
                 recettes={recettes} />)
         }
         <footer>
+          {logout}
           <button onClick={chargerExemple}>Remplir</button>
         </footer>
       </div>
