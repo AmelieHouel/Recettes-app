4cf30a7704bd76da5ab7a37fec56976850a58ee5
AmelieHouel <amelie.houel@hetic.net>
2020-05-27 12:59:41 +0200

    add CRUD recettes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,44 @@ import Admin from "./components/Admin";
 import Card from "./components/Card";
 import recettes from "./recettes";
 
+//firebase
+import base from './base'
+
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
     recettes: {},
   };
+
+  componentDidMount () {
+    this.ref = base.syncState(`/${this.state.pseudo}/recettes`, {
+      context: this,
+      state: 'recettes'
+    })
+  }
+
+  componentWillUnmount () {
+    base.removeBinding(this.ref)
+  }
+
+  ajouterRecette = recette => {
+    const recettes = { ...this.state.recettes }
+    recettes[`recette-${Date.now()}`] = recette
+    this.setState({ recettes })
+  }
+
+  majRecette = (key, newRecette) => {
+    const recettes = { ...this.state.recettes }
+    recettes[key] = newRecette
+    this.setState({ recettes })
+  }
+
+  supprimerRecette = key => {
+    const recettes = { ...this.state.recettes }
+    recettes[key] = null
+    this.setState({ recettes })
+
+  }
 
   chargerExemple = () => this.setState({ recettes });
 
@@ -21,12 +54,17 @@ class App extends Component {
     const cards = Object.keys(this.state.recettes).map((key) => (
       <Card key={key} details={this.state.recettes[key]} />
     ));
-    console.log(cards);
+
     return (
       <div className="box">
         <Header pseudo={this.state.pseudo} />
         <div className="cards">{cards}</div>
-        <Admin chargerExemple={this.chargerExemple}></Admin>
+        <Admin 
+          recettes={this.state.recettes}
+          majRecette ={this.majRecette}
+          ajouterRecette={this.ajouterRecette}
+          supprimerRecette={this.supprimerRecette}
+          chargerExemple={this.chargerExemple}></Admin>
       </div>
     );
   }
--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: 'AIzaSyDpOb9K_0smYucHFG34pB5OKHy7o1SVgGs',
+  authDomain: 'recettes-app-1ffe1.firebaseapp.com',
+  databaseURL: 'https://recettes-app-1ffe1.firebaseio.com'
 })
 
 const base = Rebase.createClass(firebaseApp.database())
